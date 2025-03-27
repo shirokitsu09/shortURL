44e3f315c2aeb9d14f384097fc8e9fb7b2bcdea6
“shirokitsu09” <somritthanapol@gmail.com>
2025-03-27 22:54:10 +0700

    fix bug

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // ใช้ Body Parser สำหรับดึงข้อมูลจาก form
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // ใช้ Static files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.get('/', (req, res) => {
